bb7a1da1a4efca934192b287fa81e9d125af94de
kareemschultz <kareemschultz46@gmail.com>
2026-01-19 10:25:41 -0400

    initial commit

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { env } from "@PeopleFlow-HR-Suite/env/web";
+import { polarClient } from "@polar-sh/better-auth";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.VITE_SERVER_URL,
+  plugins: [polarClient()],
+});
